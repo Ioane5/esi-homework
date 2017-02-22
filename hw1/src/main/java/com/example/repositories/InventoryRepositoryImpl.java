cc21cd5161cabfa7f90570f5d67daaf7592894a9
Viper <mkenc12@freeuni.edu.ge>
2017-02-22 23:46:25 +0200

    Implement Query 3

--- hw1/src/main/java/com/example/repositories/InventoryRepositoryImpl.java
+++ hw1/src/main/java/com/example/repositories/InventoryRepositoryImpl.java
@@ -1,5 +1,9 @@
 package com.example.repositories;
 
+import com.example.models.BusinessPeriod;
+import com.example.models.PlantInventoryEntry;
+import com.example.models.PlantInventoryItem;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManager;
@@ -24,5 +28,24 @@ public class InventoryRepositoryImpl implements CustomInventoryRepository {
                         .getResultList();
         selected.forEach((i) -> System.out.println(">> " + i));
         return selected;
+    }
+
+    @Override
+    public List<PlantInventoryItem> findAvailablePlantsInBusinessPeriod(BusinessPeriod period) {
+        //noinspection unchecked
+        return em.createQuery(
+                        "select p from PlantInventoryItem p where p not in (" +
+                            "select r.plant from PlantReservation r " +
+                                "where r.schedule.startDate > ?1 and r.schedule.startDate < ?2 and r.rental IS NOT NULL)")
+                        .setParameter(1, period.getStartDate())
+                        .setParameter(2, period.getEndDate())
+                        .getResultList();
+
+    }
+
+    @Override
+    public List<PlantInventoryItem> findPlantsNotHiredInLastSixMonths() {
+        BusinessPeriod period = BusinessPeriod.of(LocalDate.now().minusMonths(6), LocalDate.now());
+        return findAvailablePlantsInBusinessPeriod(period);
     }
 }
