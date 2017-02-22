e931208f05d4bb7c9df2f1b0698c4d4e2b3096e3
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2017-02-22 15:35:47 +0200

    Add minus query

--- hw1/src/main/java/com/example/repositories/InventoryRepositoryImpl.java
+++ hw1/src/main/java/com/example/repositories/InventoryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.example.repositories;
 
+import com.example.models.PlantInventoryItem;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManager;
@@ -15,14 +16,31 @@ public class InventoryRepositoryImpl implements CustomInventoryRepository {
     EntityManager em;
 
     public List findAvailablePlants(String name, LocalDate startDate, LocalDate endDate) {
-        List selected =
-                em.createQuery("select p from PlantInventoryItem p " +
+        System.out.println("findAvailablePlants() called with: name = [" + name + "], startDate = [" + startDate + "], endDate = [" + endDate + "]");
+        List<PlantInventoryItem> minus =
+                em.createQuery("select p from PlantReservation pr join pr.plant p " +
+                                "where not(pr.schedule.startDate > ?2 or pr.schedule.endDate < ?1) "
+                        , PlantInventoryItem.class)
+                        .setParameter(1, startDate)
+                        .setParameter(2, endDate)
+                        .getResultList();
+        System.out.println("Minus selected ");
+        minus.forEach((i) -> {
+            System.out.println(i);
+        });
+        System.out.println("Selected ");
+
+        List<Object[]> selected =
+                em.createQuery("select pe, count(p) from PlantInventoryItem p join p.plantInfo pe " +
                         "where p.equipmentCondition = 'SERVICEABLE' " +
-                        "and LOWER(p.plantInfo.name) like lower(?1) " +
-                        "group by p.plantInfo")
+                        "and LOWER(pe.name) like lower(?1) " +
+                        "group by pe", Object[].class)
                         .setParameter(1, name)
                         .getResultList();
-        selected.forEach((i) -> System.out.println(">> " + i));
+        selected.forEach((i) -> {
+            System.out.println(i[0]);
+            System.out.println(i[1]);
+        });
         return selected;
     }
 }
