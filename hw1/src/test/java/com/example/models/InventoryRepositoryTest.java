4682f5e9208f5d6d3724926c7a2d48d43e8f9cba
Viper <mkenc12@freeuni.edu.ge>
2017-02-22 23:46:05 +0200

    Implement tests for findPlantsNotHiredInLastSixMonths method

--- hw1/src/test/java/com/example/models/InventoryRepositoryTest.java
+++ hw1/src/test/java/com/example/models/InventoryRepositoryTest.java
@@ -2,6 +2,9 @@ package com.example.models;
 
 import com.example.MainApplication;
 import com.example.repositories.InventoryRepository;
+import com.example.repositories.PlantInventoryItemRepository;
+import com.example.repositories.PlantReservationRepository;
+import com.example.repositories.PurchaseOrderRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +25,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Sql(scripts = "/plants-dataset.sql")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class InventoryRepositoryTest {
-
     @Autowired
-    InventoryRepository inventoryRepo;
+    private InventoryRepository inventoryRepo;
+    @Autowired
+    private PurchaseOrderRepository purchaseOrderRepo;
+    @Autowired
+    private PlantReservationRepository plantReservationRepo;
+    @Autowired
+    private PlantInventoryItemRepository plantInventoryItemRepo;
 
     @Test
     public void checkTests() {
@@ -34,6 +42,32 @@ public class InventoryRepositoryTest {
     @Test
     public void findAvailableTest() {
         assertThat(inventoryRepo.findAvailablePlants("Mini excavator", LocalDate.MIN, LocalDate.MAX)).hasSize(2);
+    }
+
+    @Test
+    public void findPlantsNotHiredInLastSixMonthsWithReservationinPastTest() {
+        int expectedSize = plantInventoryItemRepo.findAll().size() - 1;
+        setUpPurchase(LocalDate.now().minusMonths(3), LocalDate.now().minusMonths(2));
+        assertThat(inventoryRepo.findPlantsNotHiredInLastSixMonths())
+                .hasSize(expectedSize);
+    }
+
+    @Test
+    public void findPlantsNotHiredInLastSixMonthsWithReservationInFutureTest() {
+        int expectedSize = plantInventoryItemRepo.findAll().size();
+        setUpPurchase(LocalDate.now().plusMonths(3), LocalDate.now().plusMonths(2));
+        assertThat(inventoryRepo.findPlantsNotHiredInLastSixMonths())
+                .hasSize(expectedSize);
+    }
+
+    private void setUpPurchase(LocalDate startDate, LocalDate endDate) {
+        PurchaseOrder po = new PurchaseOrder();
+        purchaseOrderRepo.save(po);
+        PlantReservation r = new PlantReservation();
+        r.setRental(po);
+        r.setSchedule(BusinessPeriod.of(startDate, endDate));
+        r.setPlant(plantInventoryItemRepo.findOne(1L));
+        plantReservationRepo.save(r);
     }
 }
 
