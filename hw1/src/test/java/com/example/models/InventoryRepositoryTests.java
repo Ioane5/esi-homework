bcd3ef7cc9d0775ed72e1799b0966a82005ab4b8
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2017-02-23 10:11:57 +0200

    merged with master

--- hw1/src/test/java/com/example/models/InventoryRepositoryTests.java
+++ hw1/src/test/java/com/example/models/InventoryRepositoryTests.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(classes = MainApplication.class)
 @Sql(scripts = "plants-dataset.sql")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class InventoryRepositoryTest {
+public class InventoryRepositoryTests {
 
     @Autowired
     InventoryRepository inventoryRepo;
@@ -45,7 +45,7 @@ public class InventoryRepositoryTest {
     }
 
     @Test
-    public void findAvailableTest_SelectAllWorkingdDumpers() {
+    public void findAvailableTest_SelectAllWorkingDumpers() {
         assertThat(inventoryRepo.findAvailablePlants("dumper", LocalDate.of(2017, 6, 1), LocalDate.of(2017, 9, 1)))
                 .hasSize(2);
     }
