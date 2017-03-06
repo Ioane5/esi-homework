9c0f54524e88031ad7bdab94a8fd46e5b4f66450
Meravici <giogogiashvili@gmail.com>
2017-03-06 23:33:05 +0200

    Refactor MaintenancePlan and MaintenanceTask

--- hw1/src/main/java/com/example/maintenance/domain/repository/MaintenancePlanRepository.java
+++ hw1/src/main/java/com/example/maintenance/domain/repository/MaintenancePlanRepository.java
@@ -4,9 +4,6 @@ import com.example.maintenance.domain.model.MaintenancePlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by ioane5 on 2/20/17.
- */
 @Repository
-public interface MaintenancePlanRepository extends JpaRepository<MaintenancePlan, Long> {
+public interface MaintenancePlanRepository extends JpaRepository<MaintenancePlan, String> {
 }
