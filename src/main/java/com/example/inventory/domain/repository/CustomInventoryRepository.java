f543c54b9e635421fbf08db0bdc6da7c17730c70
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2017-05-14 21:17:01 +0300

    removed obsolete repository methods

--- src/main/java/com/example/inventory/domain/repository/CustomInventoryRepository.java
+++ src/main/java/com/example/inventory/domain/repository/CustomInventoryRepository.java
@@ -2,14 +2,10 @@ package com.example.inventory.domain.repository;
 
 import com.example.common.domain.model.BusinessPeriod;
 import com.example.inventory.domain.model.PlantInventoryEntry;
-import com.example.inventory.domain.model.PlantInventoryEntryCount;
 import com.example.inventory.domain.model.PlantInventoryItem;
 
 import java.util.List;
 
-/**
- * Created by ioane5 on 2/20/17.
- */
 public interface CustomInventoryRepository {
     /**
      * Find PlantInventoryItems that are available (not rented)
@@ -19,8 +15,5 @@ public interface CustomInventoryRepository {
      */
     List<PlantInventoryItem> findAvailablePlantItemsInBusinessPeriod(String entryId, BusinessPeriod period);
     List<PlantInventoryItem> findPlantItemsNotHiredInLastSixMonths();
-    List<PlantInventoryEntryCount> findAndCountAvailablePlants(String name, BusinessPeriod period);
     List<PlantInventoryEntry> findAvailablePlants(String name, BusinessPeriod period);
-    boolean itemAvailableStrict(PlantInventoryEntry entry, BusinessPeriod period);
-    boolean itemAvailableRelaxed(PlantInventoryEntry entry, BusinessPeriod period);
 }
