13855002ee85b77b0945fb8fe7c05221b05f7755
Viper <mkenc12@freeuni.edu.ge>
2017-05-15 14:22:49 +0300

    Update (and clean up) tests

--- src/test/java/com/example/inventory/domain/repository/InventoryRepositoryTests.java
+++ src/test/java/com/example/inventory/domain/repository/InventoryRepositoryTests.java
@@ -3,11 +3,10 @@ package com.example.inventory.domain.repository;
 import com.example.MainApplication;
 import com.example.common.domain.model.BusinessPeriod;
 import com.example.common.infrastructure.IdentifierFactory;
-import com.example.inventory.domain.model.PlantInventoryEntry;
 import com.example.inventory.domain.model.PlantReservation;
 import com.example.maintenance.domain.repository.MaintenancePlanRepository;
-import com.example.sales.domain.repository.PurchaseOrderRepository;
 import com.example.sales.domain.model.PurchaseOrder;
+import com.example.sales.domain.repository.PurchaseOrderRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,28 +52,11 @@ public class InventoryRepositoryTests {
     }
 
     private void setUpPurchase(LocalDate startDate, LocalDate endDate) {
-        PurchaseOrder po = PurchaseOrder.of(IdentifierFactory.nextId(),null,null,null);
+        PurchaseOrder po = PurchaseOrder.of(IdentifierFactory.nextId(), null, null, null, null);
         purchaseOrderRepo.save(po);
         PlantReservation r = PlantReservation.of(IdentifierFactory.nextId(), BusinessPeriod.of(startDate, endDate), plantInventoryItemRepo.findOne("1"))
                 .withPurchaseOrder(po);
         plantReservationRepo.save(r);
-    }
-
-    private void setUpReservation(LocalDate startDate, LocalDate endDate, String plant_id, String maintenance_id) {
-        PlantReservation r = PlantReservation.of(IdentifierFactory.nextId(), BusinessPeriod.of(startDate, endDate), plantInventoryItemRepo.findOne(plant_id));
-
-        if(maintenance_id != null) {
-            r.withMaintenancePlan(maintenancePlanRepository.findOne(maintenance_id));
-        }
-
-        plantReservationRepo.save(r);
-    }
-
-    private void setUpReservations() {
-        setUpReservation(LocalDate.now().plusDays(13), LocalDate.now().plusDays(15), "1", null);
-
-        setUpReservation(LocalDate.now().minusDays(8), LocalDate.now().plusDays(16), "12", "1");
-        setUpReservation(LocalDate.now().plusMonths(1), LocalDate.now().plusMonths(1), "13", "1");
     }
 }
 
