5e5694bc3ae7e7bee2752bbdc05cea435c9e6b28
Viper <mkenc12@freeuni.edu.ge>
2017-05-25 22:10:47 +0300

    Add api/inventory/reservations endpoint (Ref: #58)

--- src/main/java/com/example/inventory/rest/controllers/InventoryRestController.java
+++ src/main/java/com/example/inventory/rest/controllers/InventoryRestController.java
@@ -1,16 +1,16 @@
 package com.example.inventory.rest.controllers;
 
+import com.example.common.application.exceptions.PlantNotFoundException;
+import com.example.common.application.services.BusinessPeriodAssembler;
 import com.example.common.domain.model.BusinessPeriod;
+import com.example.inventory.application.dto.MaintenanceReservationDTO;
 import com.example.inventory.application.dto.PlantInventoryEntryDTO;
 import com.example.inventory.application.services.InventoryService;
 import com.example.inventory.application.services.PlantInventoryEntryAssembler;
 import com.example.inventory.domain.model.PlantInventoryEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -22,6 +22,8 @@ public class InventoryRestController {
     private InventoryService inventoryService;
     @Autowired
     private PlantInventoryEntryAssembler plantInventoryEntryAssembler;
+    @Autowired
+    private BusinessPeriodAssembler businessPeriodAssembler;
 
     @GetMapping("/plants")
     public List<PlantInventoryEntryDTO> findAvailablePlants(
@@ -31,5 +33,16 @@ public class InventoryRestController {
     ) {
         List<PlantInventoryEntry> plants = inventoryService.findAvailablePlants(plantName, BusinessPeriod.of(startDate, endDate));
         return plantInventoryEntryAssembler.toResources(plants);
+    }
+
+    @PostMapping("/reservations")
+    public void reservePlantForMaintenance(@RequestBody MaintenanceReservationDTO reservationDTO) throws PlantNotFoundException {
+        String itemId = reservationDTO.getItemId();
+        String maintenancePlanId = reservationDTO.getMaintenancePlanId();
+        BusinessPeriod maintenancePeriod = businessPeriodAssembler.fromResource(reservationDTO.getMaintenancePeriod());
+        inventoryService.reservePlantItem(itemId, maintenancePeriod, maintenancePlanId);
+        // TODO: Should discard any existing reservations on the item with given itemId
+        // TODO: Should find replacement items if possible
+        // TODO: Should email client if replacement was not found (reject po)
     }
 }
